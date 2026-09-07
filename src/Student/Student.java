0a106742e8ee19d165df7176a7c3bd941e0fc0e0
EugenZuev <zhenya.zuev-2110@yandex.ru>
2026-09-07 23:29:02 +0300

    Book added in Student

--- src/Student/Student.java
+++ src/Student/Student.java
@@ -1,10 +1,16 @@
 package Student;
 
+import java.util.List;
+
+import Book.Book;
+
 public class Student {
 	private String name;
+	private final List<Book> books; 
 
-    public Student(String name) {
+    public Student(String name, List<Book> books) {
         this.name = name;
+        this.books = books;
     }
 
     public String getName() { return name; }
